1c7ed63c6e69ff69e318a327e2c1c0e8c72c4bbe
Uffe Arlo <uat@aperitas.com>
2024-02-28 23:00:46 +0100

    k

--- server_code/server/utils/asset_endpoint.py
+++ server_code/server/utils/asset_endpoint.py
@@ -7,62 +7,58 @@ from anvil.server import (
 class asset_endpoint:
     """Decorator for asset endpoint funcs"""
 
-    def __init__(self, type='html'):
+    def __init__(self, asset_type='html'):
         """."""
-        
-        
-        self.type = type
+        self.asset_type = asset_type
 
     def __call__(self, func):
         """."""
+        # Inspect func
         code_object = func.__code__
         pos_args_n = code_object.co_argcount
-        pos_arg_names = code_object.co_varnames[:pos_args_n]
+        # Get names of positional func parameters (incl. with default values)
+        self.param_names = code_object.co_varnames[:pos_args_n]
+        # Infer if func has a '**' parameter
+        self.accept_query = func.__code__.co_flags & 0x08 != 0
 
-
-
-        self.url = f"/{func.__name__}".replace("_", "-")
-        if pos_arg_names:
-            self.url = self.url + '/:' + '/:'.join(pos_arg_names)
-        #if accept_query is True:
-            #self.url = self.url + '/'
+        # Infer endpoint path from func name and its parameters
+        self.path = f"/{func.__name__}".replace("_", "-")
+        if self.param_names:
+            self.path = self.path + '/:' + '/:'.join(self.param_names)
+        if self.accept_query is True:
+            self.url = self.url + '/'
 
         def response_func(*params, **q):
-            """."""
-            if self.type == "css":
-                response_headers = {"Content-Type": "text/css; charset=utf-8"}
-            elif self.type == "html":
-                response_headers = {"Content-Type": "text/html; charset=utf-8"}
-            elif self.type == "jpg":
-                response_headers = {"Content-Type": "image/jpeg"}
-            elif self.type == "js":
-                response_headers = {
+            """Returns HTTP response based on func return value. Called when endpooint invoked."""
+            # Prepare HTTP response
+            http_response = _HttpResponse()
+            # Set headers based on asset type
+            if self.asset_type == "css":
+                http_response.headers = {"Content-Type": "text/css; charset=utf-8"}
+            elif self.asset_type == "html":
+                http_response.headers = {"Content-Type": "text/html; charset=utf-8"}
+            elif self.asset_type == "jpg":
+                http_response.headers = {"Content-Type": "image/jpeg"}
+            elif self.asset_type == "js":
+                http_response.headers = {
                     "Access-Control-Allow-Origin": "*",
                     "Content-Type": "application/javascript; charset=utf-8",
                 }
-            elif self.type == "json":
-                response_headers = {
+            elif self.asset_type == "json":
+                http_response.headers = {
                     "Access-Control-Allow-Origin": "*",
                     "Content-Type": "application/json; charset=utf-8",
                 }
-
             
-            http_response = _HttpResponse()
+            # Give func access to asset endpoint obj
             func._asset_endpoint = self
-            asset = func(*params, **q)
-            http_response.body = asset
 
-            http_response.headers = response_headers
+            # Set HTTP response body from func return value (the asset)
+            http_response.body = func(*params, **q)
 
             return http_response
 
-            
-
         # Register endpoint
-        
-        
-
-        _http_endpoint(self.url, methods=["GET"])(response_func)
-
+        _http_endpoint(self.path, methods=["GET"])(response_func)
         # Make 'asset_endpoint' decorator stackable
         return func

87d63b019f104ec2844f6a0ab4465d93baa67bb0
Uffe Arlo <uat@aperitas.com>
2024-02-28 15:45:42 +0100

    k

--- server_code/server/db.py
+++ server_code/server/db.py
@@ -4,5 +4,12 @@ from anvil.tables import app_tables
 def get_image(name):
     row = app_tables.images.get(name=name)
     image = row['file']
+
+    print(image.content_type)
+
+
     return image
     
+
+if __name__ == "__main__":
+    ...

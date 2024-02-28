473661d8f6fbde5b1609ccb1eb521988e77e6d51
Uffe Arlo <uat@aperitas.com>
2024-02-28 15:27:59 +0100

    k

--- server_code/server/endpoints.py
+++ server_code/server/endpoints.py
@@ -27,3 +27,27 @@ def get_image():
         
 
     return response
+
+
+
+
+@anvil.server.http_endpoint(
+    "/get-page",
+    methods=["GET"],
+)
+def get_page():
+    response = anvil.server.HttpResponse()
+    
+    response.headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "text/html; charset=utf-8"
+    }
+
+    
+
+        
+    response.body = '<h1>Hi</h1>'
+    response.status = 200
+        
+
+    return response

654f0d5f93660b4657d6bbc70bfe300d35f6b3c9
Uffe Arlo <uat@aperitas.com>
2024-02-28 21:37:57 +0100

    k

--- server_code/server/endpoints.py
+++ server_code/server/endpoints.py
@@ -51,10 +51,10 @@ def get_image(name):
     "/get-image",
     methods=["GET"],
 )
-def get_image(*args, **kwargs):
+def get_image(**kwargs):
 
     print('kwargs:', kwargs)##
-    print('args:', args)##
+    ##print('args:', args)##
 
     response = create_response('jpg')
     image = _get_image('test')
